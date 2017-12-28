327e8499e77e10b1f16ffde566709c2c6f03ce31
Phil Burk <philburk@mobileer.com>
2017-12-27 16:55:05 -0800

    MusicKeyboardView: clip negative x or y
    They were causing ArrayIndexOutOfBoundsException
    on some devices.
    
    Fixes #40

--- MidiTools/src/main/java/com/mobileer/miditools/MusicKeyboardView.java
+++ MidiTools/src/main/java/com/mobileer/miditools/MusicKeyboardView.java
@@ -220,6 +220,9 @@ public class MusicKeyboardView extends View {
         // Get the pointer's current position
         float x = event.getX(pointerIndex);
         float y = event.getY(pointerIndex);
+        // Some devices can return negative x or y, which can cause an array exception.
+        if (x < 0.0f) x = 0.0f;
+        if (y < 0.0f) y = 0.0f;
         boolean handled =  false;
         switch (action) {
             case MotionEvent.ACTION_DOWN:
@@ -233,6 +236,8 @@ public class MusicKeyboardView extends View {
                     id = event.getPointerId(i);
                     x = event.getX(i);
                     y = event.getY(i);
+                    if (x < 0.0f) x = 0.0f;
+                    if (y < 0.0f) y = 0.0f;
                     onFingerMove(id, x, y);
                 }
                 handled = true;
